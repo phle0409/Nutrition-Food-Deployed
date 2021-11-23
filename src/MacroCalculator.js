9eeca81c60b28889d9a11e328301969f2951f096
Minh Le <phle0409@gmail.com>
2021-11-22 20:03:21 -0800

    Macro calculator

---
+++ src/MacroCalculator.js
@@ -0,0 +1,33 @@
+import { Stack } from "react-bootstrap";
+import AppSearchFood from "./AppSearchFood";
+import AppTDEE from "./AppTDEE";
+import { useState } from "react";
+
+const MacroCalculator = () => {
+  const [tdee, setTDEE] = useState('TBD')
+
+  return (
+    <div>
+      <h4>Total Daily Energy Expenditure(TDEE)</h4>
+      <Stack direction="horizontal" gap={3}>
+        <Stack className="border p-3 align-self-start w-50">
+          <AppTDEE onTDEE={setTDEE} />
+        </Stack>
+
+        <Stack className="align-self-start w-50">
+          <Stack className="border p-3 text-center">
+            <h4>Maintenance Calories</h4>
+            <h4 style={{
+              color: "red"
+            }}>{tdee}</h4>
+            <h4>Calories per day</h4>
+          </Stack>
+
+          {/* <AppSearchFood /> */}
+        </Stack>
+      </Stack>
+    </div>
+  );
+}
+
+export default MacroCalculator;
