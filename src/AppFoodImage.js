c2bd7728a887c89c246456e2cb2b2ec63b2c6880
Minh Le <phle0409@gmail.com>
2021-11-22 17:07:01 -0800

    Add AppImage for gallery

---
+++ src/AppFoodImage.js
@@ -0,0 +1,55 @@
+import usePixaBay from "./usePixaBay";
+import usePexel from "./usePexel";
+import { Placeholder, Figure, Col, Stack } from "react-bootstrap";
+import { run as runHolder } from 'holderjs/holder';
+import { useEffect } from 'react';
+
+const AppFoodImage = (props) => {
+  useEffect(() => {
+    runHolder('image-holder');
+  });
+
+  const name = props.name;
+  const { data, isPending, empty } = usePixaBay(name, 3)
+
+  return (
+    <>
+      {isPending &&
+        <Col>
+          <Placeholder as="p" animation="glow">
+            <Placeholder xs={12} />
+          </Placeholder>
+        </Col>
+      }
+
+      {empty &&
+        <Col>
+          <Stack>
+            <Figure>
+              <Figure.Image className="image-holder" src="holder.js/100px180?text=No%20Image" thumbnail />
+              <Figure.Caption>{name}</Figure.Caption>
+            </Figure>
+          </Stack>
+        </Col>
+      }
+
+      {data.length > 0 &&
+        <Col>
+          <Stack>
+            <Figure>
+              <Figure.Image src={data[0]} thumbnail style={{
+                height: "180px",
+                width: "auto",
+                objectFit: "cover"
+              }} />
+              <Figure.Caption>{name}</Figure.Caption>
+            </Figure>
+          </Stack>
+        </Col>
+      }
+
+    </>
+  );
+}
+
+export default AppFoodImage;
