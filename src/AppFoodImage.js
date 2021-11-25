74461d4d0ee0c4d38a5c95565a38b40c06860e28
Minh Le <phle0409@gmail.com>
2021-11-25 04:41:35 -0800

    Update no Image

--- src/AppFoodImage.js
+++ src/AppFoodImage.js
@@ -2,6 +2,7 @@ import usePixaBay from "./usePixaBay";
 import { Placeholder, Figure, Col, Stack } from "react-bootstrap";
 import { run as runHolder } from 'holderjs/holder';
 import { useEffect } from 'react';
+import usePexel from "./usePexel";
 
 const AppFoodImage = (props) => {
   useEffect(() => {
@@ -10,6 +11,32 @@ const AppFoodImage = (props) => {
 
   const name = props.name;
   const { data, isPending, empty } = usePixaBay(name, 3)
+
+  const filterEmptyImage = () => {
+    if (empty) {
+      return <Col>
+        <Stack>
+          <Figure>
+            <Figure.Image className="image-holder" src="holder.js/100px180?text=No%20Image" thumbnail />
+            <Figure.Caption>{name}</Figure.Caption>
+          </Figure>
+        </Stack>
+      </Col>
+    } else {
+      return < Col >
+        <Stack>
+          <Figure>
+            <Figure.Image src={data[0]} thumbnail style={{
+              height: "180px",
+              width: "auto",
+              objectFit: "cover"
+            }} />
+            <Figure.Caption>{name}</Figure.Caption>
+          </Figure>
+        </Stack>
+      </Col>
+    }
+  }
 
   return (
     <>
@@ -21,31 +48,7 @@ const AppFoodImage = (props) => {
         </Col>
       }
 
-      {/* {empty &&
-        <Col>
-          <Stack>
-            <Figure>
-              <Figure.Image className="image-holder" src="holder.js/100px180?text=No%20Image" thumbnail />
-              <Figure.Caption>{name}</Figure.Caption>
-            </Figure>
-          </Stack>
-        </Col>
-      } */}
-
-      {data.length > 0 &&
-        <Col>
-          <Stack>
-            <Figure>
-              <Figure.Image src={data[0]} thumbnail style={{
-                height: "180px",
-                width: "auto",
-                objectFit: "cover"
-              }} />
-              <Figure.Caption>{name}</Figure.Caption>
-            </Figure>
-          </Stack>
-        </Col>
-      }
+      {data.length > 0 && filterEmptyImage()}
 
     </>
   );
